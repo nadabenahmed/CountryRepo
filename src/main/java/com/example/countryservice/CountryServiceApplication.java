21c09ea3bf0b27eedacc4a2e2a182ef41de0a115
hasse <hasse@NADA.mshome.net>
2026-04-16 16:38:07 +0100

    Update Jenkinsfile and pom.xml for Tomcat 10

--- src/main/java/com/example/countryservice/CountryServiceApplication.java
+++ src/main/java/com/example/countryservice/CountryServiceApplication.java
@@ -2,16 +2,9 @@ package com.example.countryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CountryServiceApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CountryServiceApplication.class);
-    }
+public class CountryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CountryServiceApplication.class, args);
